3399cea47deea074c8091e2161a303eca0bb71bb
Mehtiuo <mahdi.nedaee4@gmail.com>
2025-02-09 20:54:56 +0330

    ADD commands folder to project (it's a good thing for me that forget commands everytime)

---
+++ commands/nest.ps1
@@ -0,0 +1,11 @@
+# nest.js help command
+nest --help
+
+# nest generate module
+nest g mo [name]
+
+# nest generate controller
+nest g co [name]
+
+# nest generate service
+nest g s [name]
---
+++ commands/prisma.ps1
@@ -0,0 +1,8 @@
+# prisma generate schema
+npx prisma init
+
+# push schema in database
+npm prisma db schema
+
+# prisma studio and managment models
+npx prisma studio
